ecd8c074ac1e4b6adb05f37403acb23742c6380d
Quentin Brault <tititesouris@laposte.net>
2015-09-24 12:03:13 +0200

    First

---
+++ src/angrybirds/Main.java
@@ -0,0 +1,12 @@
+package angrybirds;
+
+/**
+ * Created by Quentin Brault on 24/09/2015.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
